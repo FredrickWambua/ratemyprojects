cc09c9d490d7c4f51955a1b967d832e298358bc8
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-23 17:19:17 +0300

    sets up models and tests

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+
+
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    '''
+    Custom user manager where email is the unique identifiers
+    for authentication instead of username
+    '''
+    def create_user(self, email, password, **kwargs):
+        '''
+        Create and save a User with the given email and password.
+        '''
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password =(password)
+        user.save()
+        return user
+    def create_superuser(self, email, password, **kwargs):
+        '''
+        Create and save a superuser with the given email and password.
+        '''
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', True)
+        kwargs.setdefault('is_active', True)
+
+        if kwargs.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True'))
+        if kwargs.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True'))
+        return self.create_user(email, password, **kwargs)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+
+    def __str__(self):
+        return self.email
