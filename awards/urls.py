9a16e71fadc7bb2a3fcaee1c6110dd242b982a10
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-27 12:36:35 +0300

    adds url path for register view

--- awards/urls.py
+++ awards/urls.py
@@ -5,12 +5,14 @@ from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('upload-project/', views.UploadProject, name='project-upload'),
     path('myprofile/', views.UserProfile, name='myprofile'),
     path('profile/', views.profile, name='profile'),
     path('signup/', views.signup, name='signup'),
+    path('register', views.RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='registrations/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='registrations/logout.html'), name='logout'),
     path('profile-list/', views.profileList, name='profile-list'),
