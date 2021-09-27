e503512f11d8d6c45c82cc5d4d21204f08d43373
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-27 06:30:37 +0300

    modifies signals

--- awards/signals.py
+++ awards/signals.py
@@ -1,18 +1,14 @@
-from awards.models import  Profile
+from awards.models import  CustomUser, Profile 
 from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth.models import User
 
 
-@receiver(post_save,sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 
-@receiver(post_save,sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
-post_save.connect(create_user_profile, sender=User)
-post_save.connect(save_user_profile, sender=User)
+post_save.connect(create_user_profile, sender=CustomUser)
+post_save.connect(save_user_profile, sender=CustomUser)
