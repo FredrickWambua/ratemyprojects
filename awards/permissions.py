bb13e0bdf184c83e98ae53042c6880f95c77f5aa
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-27 17:18:07 +0300

    api authentication

--- awards/permissions.py
+++ awards/permissions.py
@@ -1,8 +1,26 @@
-from rest_framework.permissions import SAFE_METHODS, BasePermission
+import jwt
+from rest_framework import authentication, exceptions
+from django.conf import settings
+from django.contrib.auth.models import User
 
-class IsAdminOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
-        else:
-            return request.user.is_staff
+class JWTAuthentication(authentication.BaseAuthentication):
+    def authenticate(self, request):
+        auth_data = authentication.get_authorization_header(request)
+
+        if not auth_data:
+            return None
+
+        prefix, token = auth_data.decode('utf-8').split(' ')
+
+        try:
+            payload = jwt.decode(token, settings.JWT_SECRET_KEY)
+
+            user = User.objects.get(username=payload['username'])
+            return (user, token)
+
+        except jwt.DecodeError as identifier:
+            raise exceptions.AuthenticationFailed('Your token is invalid, login')
+        except jwt.ExpiredSignatureError as identifier:
+            raise exceptions.AuthenticationFailed('Your token is expired, login')
+
+        return super().authenticate(request)
